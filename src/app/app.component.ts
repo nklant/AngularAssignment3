78fdb4dcf528a12c2742529fc622888c59b9c9fa
Nikolay Antonov <nikolay.antonov@globalcons.com>
2018-02-23 15:43:07 +0200

    38. Outputting Lists with ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ export class AppComponent {
 
   onTogglePassword() {
     this.displayPassword = !this.displayPassword;
-    this.log.push(this.log.length + 1);
+    // this.log.push(this.log.length + 1);
+    this.log.push(new Date());
   }
 }
